1134185166a0513a459f84fb7e990a7dba045b1d
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-08 13:11:32 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ryan Hanson";
+    int age = 23;
+    String state = "Colorado";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
