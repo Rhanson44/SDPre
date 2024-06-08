9a5556da6121821b12cf4a8eca0100f29f004f19
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-08 13:13:34 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year = 2001;
+    String month = "May";
+    Boolean weekend = true;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
